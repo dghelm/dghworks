76ebf100c42d4457fdcc33e4e745ac2e2b0534fe
Daniel Helm <dan.helm@gmail.com>
2022-08-20 19:31:48 -0500

    first draft

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      //       fontFamily: {
+      //         sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
+      //       },
+      spacing: {
+        18: '4.5rem',
+        112: '28rem',
+        120: '30rem',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
+};
