459750455b5c6772235432b3c7a5222e48ca21b8
Daniel Helm <dan.helm@gmail.com>
2022-08-25 12:46:55 -0500

    use Open Sans

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      //       fontFamily: {
-      //         sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
-      //       },
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
       spacing: {
         18: '4.5rem',
         112: '28rem',
